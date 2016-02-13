82af7139f43edf31b04fa586fc4455a4b880cc10
filtercake <fabian@superpursuitmo.de>
2016-02-13 07:09:49 +0100

    fix build and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf _site
+# git clone git@github.com:filtercake/filtercake.github.io.git _site --branch master --depth 1
 mkdir _site
 cp -r .git _site
 cd _site
@@ -8,7 +9,7 @@ cd _site
 git reset --hard && git clean -df
 
 git co master
-
+git pull
 # git reset --hard && git clean -df
 # git co master
 cd ..
@@ -20,3 +21,7 @@ git add .
 git ci -m 'build'
 git push
 cd ..
+
+
+
+# git clone git@github.com:filtercake/filtercake.github.io.git _site --branch master --depth 1
